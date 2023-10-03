8799c38556d1d65964b6f204a861cee3c98a1561
AndysMac <andyliu3411@gmail.com>
2023-10-03 13:24:44 -0400

    October 3rd

---
+++ iterations/factorial calculator.py
@@ -0,0 +1,15 @@
+'''
+product = 1
+num = int(input("Enter a number: "))
+for i in range(1, num + 1):
+    product *= i
+print(f'The factorial of {num} is {product}.')
+'''
+end_result = 1
+product = 1
+num = int(input("Enter a number: "))
+while product <= num:
+    end_result *= product
+    product = product + 1
+
+print(f'The factorial of {num} is {end_result}.')
