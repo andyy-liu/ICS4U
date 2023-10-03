5e83f89c568a343b3b8eb5d14ebb690199d52f31
AndysMac <andyliu3411@gmail.com>
2023-10-03 13:01:21 -0400

    october 3rd

---
+++ iterations/FizzBuzz.py
@@ -0,0 +1,22 @@
+'''
+number = 1
+
+while number <= 50:
+    if number % 15 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    number += 1
+'''
+
+for number in range(1,51):
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
